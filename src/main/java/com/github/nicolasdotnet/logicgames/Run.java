9654e891b7b07aa72f87b88e9f7440baf4e00cd8
nicolasdotnet <nicolas.desdevises@gmail.com>
2019-03-12 11:52:41 +0100

    WindowSOurce creation

---
+++ src/main/java/com/github/nicolasdotnet/logicgames/Run.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.nicolasdotnet.logicgames;
+
+import com.github.nicolasdotnet.view.WindowHome;
+
+/**
+ *
+ * @author nicolasdotnet
+ * @version Alpha
+ * @since 2019
+ */
+public class Run {
+    
+    /**
+     * Run app LogicGames
+     * 
+     * @param args : default param
+     */
+    public static void main(String[] args) {
+        
+        WindowHome LogicGames = new WindowHome();
+        
+    }
+    
+}
---
+++ src/main/java/com/github/nicolasdotnet/view/WindowSource.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.nicolasdotnet.view;
+
+import javax.swing.JFrame;
+
+/**
+ * WindowSource est la classe qui représente le squelette d'un fenêtre
+ * @author nicolasdotnet
+ * @version Alpha
+ * @since 2019
+ */
+public class WindowSource extends JFrame {
+
+    public WindowSource() {
+        super("Jeux de logique");
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(600, 400);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        
+    }
+
+}
