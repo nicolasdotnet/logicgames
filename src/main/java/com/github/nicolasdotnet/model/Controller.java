2a8c63c3700a10bf7a65c95c01f975ccfab4aa58
nicolasdotnet <nicolas.desdevises@gmail.com>
2019-03-14 12:21:52 +0100

    add Controller.java

---
+++ src/main/java/com/github/nicolasdotnet/model/Controller.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.nicolasdotnet.model;
+
+/**
+ *
+ * Controller est la classe qui oriente vers le bon jeux
+ *
+ * @author nicolasdotnet
+ * @version Alpha
+ * @since 2019
+ */
+public class Controller {
+    
+    // le jeu avec l'option
+    
+    
+    
+}
